7d9edff23d577b74e321a8ed1529c77313fd6015
dianapnelson <145507585+dianapnelson@users.noreply.github.com>
2023-10-06 20:48:08 -0600

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,1 +1,13 @@
+#Parte 2
 
+from readchar import readkey, key 
+
+print("¡Hola, Jugador! Para jugar, presiona cualquier tecla")
+print("Si deseas parar, presiona la techa hacia arriba (↑)")
+
+while True:
+    tecla = readkey()
+    if tecla == key.UP:
+        print ("Haz finalizado el juego")
+        break
+    print(f"Haz presionado la tecla: {tecla}")
